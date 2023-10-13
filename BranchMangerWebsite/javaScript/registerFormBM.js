8b30a76e4cb5d095c985d45c039e8b2ece469a29
Futoon2002 <fetoonalsulami@gmail.com>
2023-10-13 17:42:16 +0300

    register form js page

--- BranchMangerWebsite/javaScript/registerFormBM.js
+++
@@ -1,9 +0,0 @@
-function validateForm() {
-    const firtsName = document.getElementById("FirstName").value;
-    const lastName = document.getElementById("LastName").value;
-    const email = document.getElementById("Email").value;
-    const stationName = document.getElementById("StationName").value;
-    const stationLocation = document.getElementById("GoogleMapUrl").value;
-
-
-}
